37a360a448b0cbb592297317cfea9160f44c25b1
SzymonSt <szymonst2808@MacBook-Pro-Szymon.local>
2024-04-04 02:16:12 +0200

    feat: code gen replacement

--- signal0ne/src/extension.ts
+++ signal0ne/src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import axios from 'axios';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -18,6 +19,18 @@ export function activate(context: vscode.ExtensionContext) {
 		let editor = vscode.window.activeTextEditor;
 		let selection = editor?.selection;
 		let codeSnippet = editor?.document.getText(selection);
+		let lang = editor?.document.languageId;
+		let codeSnippetContext = {
+			code: codeSnippet,
+			lang: lang
+		}
+		axios.post('http://localhost:8080/api/issues/d732e352-7bd6-4e77-987e-f52bf385f71d/add-code-as-context', codeSnippetContext)
+		.then((response: any) => {
+			console.log(response);
+			editor?.edit((editBuilder) => {
+				editBuilder.replace(selection ?? new vscode.Selection(0, 0, 0, 0), response.data.newCode);
+			})
+		});
 	});
 
 	context.subscriptions.push(codeContextDisposable);
