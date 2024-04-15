b95687c83eca29fd56fb0508e9a4f4dd412170d3
SzymonSt <szymonst2808@MacBook-Pro-Szymon.local>
2024-04-15 23:45:04 +0200

    token refresh

--- signal0ne/src/auth/signal0ne.provider.ts
+++ signal0ne/src/auth/signal0ne.provider.ts
@@ -1,9 +1,12 @@
 import * as vsc from 'vscode';
 import {getPort} from 'get-port-please';
 import * as uuid from 'uuid';
+import { API_URL } from '../extension';
 
-const AUTH_URL = 'http://localhost:8088';
+const AUTH_URL = 'http://localhost:37001';
+const AUTH_API_URL = `${API_URL}/auth`;
 const SESSION_SECRET_KEY = 'signal0ne.sessions';
+const REFRESH_TOKEN_KEY = 'signal0ne.refresh_token';
 
 export class Signal0neProvider implements vsc.AuthenticationProvider, vsc.Disposable{
 
@@ -25,15 +28,16 @@ export class Signal0neProvider implements vsc.AuthenticationProvider, vsc.Dispos
         const port = await getPort();
         const uri = vsc.Uri.parse(`${AUTH_URL}/login?callbackUrl=http://localhost:${port}`);
         vsc.env.openExternal(uri);
-        const token = await this.login(port);
+        const tokenPair = await this.login(port);
         const session: vsc.AuthenticationSession = {
             id: uuid.v4(),
-            accessToken: token,
+            accessToken: tokenPair.accessToken,
             scopes: scopes,
             //To be parametrized with user details
             account: {label: 'Signal0ne', id: 'signal0ne'},
         }
         await this.context.secrets.store(SESSION_SECRET_KEY, JSON.stringify([session]));
+        await this.context.secrets.store(REFRESH_TOKEN_KEY, tokenPair.refreshToken);
         this._sessionChangeEmitter.fire({added: [session], removed: [], changed: []});
         console.log('Session created', session.accessToken);
         return session;
@@ -44,6 +48,14 @@ export class Signal0neProvider implements vsc.AuthenticationProvider, vsc.Dispos
     }
 
     public async removeSession(sessionId: string): Promise<void> {
+        const sessions = await this.getSessions();
+        const sessionIndex = sessions.findIndex(session => session.id === sessionId);
+        if(sessionIndex !== -1){
+          var session = sessions[sessionIndex];
+            var newSessions = sessions.slice(0, sessionIndex).concat(sessions.slice(sessionIndex + 1));
+            await this.context.secrets.store(SESSION_SECRET_KEY, JSON.stringify(newSessions));
+            this._sessionChangeEmitter.fire({added: [], removed: [session], changed: []});
+        }
         return;
     }
 
@@ -55,12 +67,51 @@ export class Signal0neProvider implements vsc.AuthenticationProvider, vsc.Dispos
         return [];
     }
 
-    async login(port: any): Promise<string> {
-        const token = await new Promise<string>(async (resolve, reject) => {
+    public async refreshSession(session: vsc.AuthenticationSession, scopes: readonly string[]): Promise<vsc.AuthenticationSession> {
+        const refreshToken = await this.context.secrets.get(REFRESH_TOKEN_KEY);
+        if(refreshToken){
+          const response = await fetch(`${AUTH_API_URL}/token/refresh`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              refreshToken: refreshToken
+            })
+          });
+          if(response.ok){
+            const tokenPair: any = await response.json();
+            this.removeSession(session.id);
+            const newSession: vsc.AuthenticationSession = {
+              id: uuid.v4(),
+              accessToken: tokenPair.accessToken,
+              scopes: scopes,
+              account: {label: 'Signal0ne', id: 'signal0ne'}
+            }
+            await this.context.secrets.store(SESSION_SECRET_KEY, JSON.stringify([newSession]));
+            await this.context.secrets.store(REFRESH_TOKEN_KEY, tokenPair.refreshToken);
+            this._sessionChangeEmitter.fire({added: [newSession], removed: [], changed: []});
+            return newSession;
+          } else {
+            throw new Error('Failed to refresh session');
+          }
+        }
+        return session;
+    }
+
+    async login(port: any): Promise<any> {
+        const token = await new Promise<any>(async (resolve, reject) => {
             var server = require('http').createServer(async (req: any, res: any) => {
-              const requestToken = new URL(req.url, 'http://localhost').searchParams.get('access_token');
+              const url = new URL(req.url, 'http://localhost');
+              const requestToken = url.searchParams.get('access_token');
+              const refreshToken = url.searchParams.get('refresh_token');
               if(requestToken){
-                resolve(requestToken);
+                resolve(
+                  {
+                    requestToken: requestToken, 
+                    refreshToken: refreshToken
+                  }
+                );
                 res.end('Login successful! You can now close this tab.');
                 server.close();
               }else{
@@ -70,7 +121,10 @@ export class Signal0neProvider implements vsc.AuthenticationProvider, vsc.Dispos
               }
             }).listen(port);
           });
-        return token;
+        return {
+          accessToken: token.requestToken,
+          refreshToken: token.refreshToken
+        };
     }
 
     public async dispose() {
