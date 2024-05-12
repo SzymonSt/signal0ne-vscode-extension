7b611897d5cd83cf29a6c8629b9b1c254c8b1008
MRadzynski <radzynski.milosz@gmail.com>
2024-05-12 13:30:49 +0200

    General code refactor

--- signal0ne/src/auth/signal0ne.provider.ts
+++ signal0ne/src/auth/signal0ne.provider.ts
@@ -1,160 +1,211 @@
-import * as vsc from 'vscode';
-import {getPort} from 'get-port-please';
 import * as uuid from 'uuid';
-import { API_URL } from '../const';
-import { jwtDecode, JwtPayload } from 'jwt-decode';
-import { exec } from 'child_process';
+import * as vscode from 'vscode';
+import { API_URL } from '../data/const';
+import { getPort } from 'get-port-please';
+import { IncomingMessage, ServerResponse } from 'http';
+import { jwtDecode } from 'jwt-decode';
+import { Signal0neJwtPayload, TokenPair } from '../types/auth';
 
 const AUTH_URL = 'http://localhost:37003';
 const AUTH_API_URL = `${API_URL}/auth`;
-const SESSION_SECRET_KEY = 'signal0ne.sessions';
 const REFRESH_TOKEN_KEY = 'signal0ne.refresh_token';
+const SESSION_SECRET_KEY = 'signal0ne.sessions';
 
-interface Signal0neJwtPayload extends JwtPayload {
-  exp: number;
-  id: string;
-  userName: string;
-}
+export class Signal0neProvider
+  implements vscode.AuthenticationProvider, vscode.Disposable
+{
+  private _disposable: vscode.Disposable;
+  private _onDidAuthenticate = new vscode.EventEmitter<void>();
+  private _onDidLogout = new vscode.EventEmitter<void>();
+  private _sessionChangeEmitter =
+    new vscode.EventEmitter<vscode.AuthenticationProviderAuthenticationSessionsChangeEvent>();
+  readonly onDidAuthenticate: vscode.Event<void> =
+    this._onDidAuthenticate.event;
+  readonly onDidLogout: vscode.Event<void> = this._onDidLogout.event;
 
-export class Signal0neProvider implements vsc.AuthenticationProvider, vsc.Disposable{
+  private tokenPair: TokenPair | undefined;
 
-    private _sessionChangeEmitter = new vsc.EventEmitter<vsc.AuthenticationProviderAuthenticationSessionsChangeEvent>();
-    private _disposable: vsc.Disposable;
-
-    private _onDidAuthenticate = new vsc.EventEmitter<void>();
-    readonly onDidAuthenticate: vsc.Event<void> = this._onDidAuthenticate.event;
-    private _onDidLogout = new vsc.EventEmitter<void>();
-    readonly onDidLogout: vsc.Event<void> = this._onDidLogout.event;
-
-    private TokenPair: any;
-
-    constructor(private context: vsc.ExtensionContext) {
-      this._disposable = vsc.Disposable.from(
-        vsc.authentication.registerAuthenticationProvider('signal0ne', 'Signal0ne', this, {supportsMultipleAccounts: false})
+  constructor(private context: vscode.ExtensionContext) {
+    this._disposable = vscode.Disposable.from(
+      vscode.authentication.registerAuthenticationProvider(
+        'signal0ne',
+        'Signal0ne',
+        this,
+        { supportsMultipleAccounts: false }
       )
-    }
+    );
+  }
 
-    get onDidChangeSessions() {
-      return this._sessionChangeEmitter.event;
-    }
+  get onDidChangeSessions() {
+    return this._sessionChangeEmitter.event;
+  }
 
-    public async loginInitialSession(): Promise<void> {
-      try{
-        const port = await getPort();
-        const uri = vsc.Uri.parse(`${AUTH_URL}/login?callbackUrl=http://localhost:${port}`);
-        vsc.env.openExternal(uri);
-        this.TokenPair = await this.login(port);
-        await this.createSession([]);
-      }catch(err){
-        vsc.window.showErrorMessage(`Sign in failed: ${err}`);
-        throw err;
-      }
+  async login(port: number): Promise<TokenPair> {
+    const token = await new Promise<TokenPair>(async (resolve, reject) => {
+      const server = require('http')
+        .createServer(async (req: IncomingMessage, res: ServerResponse) => {
+          const url = new URL(req.url!, 'http://localhost');
+          const accessToken = url.searchParams.get('access_token');
+          const refreshToken = url.searchParams.get('refresh_token');
 
-    }
+          if (accessToken && refreshToken) {
+            resolve({
+              accessToken,
+              refreshToken
+            });
 
-    public async logout(): Promise<void> {
-      const sessions = await this.getSessions();
-      if (sessions[0]) {
-        this.removeSession(sessions[0].id);
-        this._onDidLogout.fire();
-      }
-    }
-
-    public async createSession(scopes: readonly string[]): Promise<vsc.AuthenticationSession> {
-      try{
-        var decodedToken = jwtDecode<Signal0neJwtPayload>(this.TokenPair.accessToken);
-        const session: vsc.AuthenticationSession = {
-            id: uuid.v4(),
-            accessToken: this.TokenPair.accessToken,
-            scopes: scopes,
-            //To be parametrized with user details
-            account: {label: decodedToken.userName, id: decodedToken.id},
-        }
-        await this.context.secrets.store(SESSION_SECRET_KEY, JSON.stringify([session]));
-        await this.context.secrets.store(REFRESH_TOKEN_KEY, this.TokenPair.refreshToken);
-        this._sessionChangeEmitter.fire({added: [session], removed: [], changed: []});
-        this._onDidAuthenticate.fire();
-        console.log('Session created', session.accessToken);
-        return session;
-      }catch(err){
-        vsc.window.showErrorMessage(`Sign in failed: ${err}`);
-        throw err;
-      }
-    }
-
-    public async removeSession(sessionId: string): Promise<void> {
-        const sessions = await this.getSessions();
-        const sessionIndex = sessions.findIndex(session => session.id === sessionId);
-        if(sessionIndex !== -1){
-          var session = sessions[sessionIndex];
-            var newSessions = sessions.slice(0, sessionIndex).concat(sessions.slice(sessionIndex + 1));
-            await this.context.secrets.store(SESSION_SECRET_KEY, JSON.stringify(newSessions));
-            this._sessionChangeEmitter.fire({added: [], removed: [session], changed: []});
-        }
-        return;
-    }
-
-    public async getSessions(scopes?: readonly string[] | undefined): Promise<readonly vsc.AuthenticationSession[]> {
-        const sessionData = await this.context.secrets.get(SESSION_SECRET_KEY);
-        if(sessionData){
-            return JSON.parse(sessionData) as vsc.AuthenticationSession[];
-        }
-        return [];
-    }
-
-    public async refreshSession(session: vsc.AuthenticationSession, scopes: readonly string[]): Promise<vsc.AuthenticationSession> {
-        const refreshToken = await this.context.secrets.get(REFRESH_TOKEN_KEY);
-        console.log('Refreshing session...');
-        if(refreshToken){
-          const response = await fetch(`${AUTH_API_URL}/token/refresh`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              refreshToken: refreshToken
-            })
-          });
-          if(response.ok){
-            this.TokenPair = await response.json();
-            await this.removeSession(session.id);
-            return await this.createSession(scopes);
+            res.end('Login successful! You can now close this tab.');
+            server.close();
           } else {
-            throw new Error('Failed to refresh session');
+            reject('Login failed! Please try again.');
+
+            res.end('Login failed! Please try again.');
+            server.close();
           }
-        }
-        return session;
+        })
+        .listen(port);
+    });
+
+    return {
+      accessToken: token.accessToken,
+      refreshToken: token.refreshToken
+    };
+  }
+
+  public async createSession(
+    scopes: readonly string[]
+  ): Promise<vscode.AuthenticationSession> {
+    try {
+      if (!this.tokenPair) throw new Error('Token pair is not available');
+
+      const decodedToken = jwtDecode<Signal0neJwtPayload>(
+        this.tokenPair.accessToken
+      );
+      const session: vscode.AuthenticationSession = {
+        accessToken: this.tokenPair.accessToken,
+        account: { id: decodedToken.id, label: decodedToken.userName },
+        id: uuid.v4(),
+        scopes
+        //To be parametrized with user details
+      };
+
+      await this.context.secrets.store(
+        SESSION_SECRET_KEY,
+        JSON.stringify([session])
+      );
+
+      await this.context.secrets.store(
+        REFRESH_TOKEN_KEY,
+        this.tokenPair.refreshToken
+      );
+
+      this._sessionChangeEmitter.fire({
+        added: [session],
+        changed: [],
+        removed: []
+      });
+
+      this._onDidAuthenticate.fire();
+
+      console.log('Session created', session.accessToken);
+      return session;
+    } catch (err) {
+      vscode.window.showErrorMessage(`Failed to sign in: ${err}`);
+      throw err;
+    }
+  }
+
+  public async dispose() {
+    this._disposable.dispose();
+  }
+
+  public async getSessions(): Promise<readonly vscode.AuthenticationSession[]> {
+    const sessionData = await this.context.secrets.get(SESSION_SECRET_KEY);
+
+    if (sessionData) {
+      return JSON.parse(sessionData) as vscode.AuthenticationSession[];
     }
 
-    async login(port: any): Promise<any> {
-        const token = await new Promise<any>(async (resolve, reject) => {
-            var server = require('http').createServer(async (req: any, res: any) => {
-              const url = new URL(req.url, 'http://localhost');
-              const requestToken = url.searchParams.get('access_token');
-              const refreshToken = url.searchParams.get('refresh_token');
-              if(requestToken){
-                resolve(
-                  {
-                    requestToken: requestToken, 
-                    refreshToken: refreshToken
-                  }
-                );
-                res.end('Login successful! You can now close this tab.');
-                server.close();
-              }else{
-                reject('Login failed! Please try again.')
-                res.end('Login failed! Please try again.');
-                server.close();
-              }
-            }).listen(port);
-          });
-        return {
-          accessToken: token.requestToken,
-          refreshToken: token.refreshToken
-        };
+    return [];
+  }
+
+  public async loginInitialSession(): Promise<void> {
+    try {
+      const port = await getPort();
+      const uri = vscode.Uri.parse(
+        `${AUTH_URL}/login?callbackUrl=http://localhost:${port}`
+      );
+
+      vscode.env.openExternal(uri);
+
+      this.tokenPair = await this.login(port);
+      await this.createSession([]);
+    } catch (err) {
+      vscode.window.showErrorMessage(`Failed to sign in: ${err}`);
+      throw err;
+    }
+  }
+
+  public async logout() {
+    const sessions = await this.getSessions();
+
+    if (sessions[0]) {
+      this.removeSession(sessions[0].id);
+      this._onDidLogout.fire();
+    }
+  }
+
+  public async refreshSession(
+    session: vscode.AuthenticationSession
+  ): Promise<vscode.AuthenticationSession> {
+    const refreshToken = await this.context.secrets.get(REFRESH_TOKEN_KEY);
+
+    if (refreshToken) {
+      const options = {
+        body: JSON.stringify({
+          refreshToken
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      };
+
+      const response = await fetch(`${AUTH_API_URL}/token/refresh`, options);
+
+      if (response.ok) {
+        this.tokenPair = (await response.json()) as TokenPair;
+        await this.removeSession(session.id);
+
+        return await this.createSession(session.scopes);
+      } else {
+        throw new Error('Failed to refresh the session');
+      }
     }
 
-    public async dispose() {
-      this._disposable.dispose();
+    return session;
+  }
+
+  public async removeSession(sessionId: string) {
+    const sessions = await this.getSessions();
+    const sessionIndex = sessions.findIndex(({ id }) => id === sessionId);
+
+    if (sessionIndex !== -1) {
+      const session = sessions[sessionIndex];
+      const newSessions = sessions
+        .slice(0, sessionIndex)
+        .concat(sessions.slice(sessionIndex + 1));
+
+      await this.context.secrets.store(
+        SESSION_SECRET_KEY,
+        JSON.stringify(newSessions)
+      );
+
+      this._sessionChangeEmitter.fire({
+        added: [],
+        changed: [],
+        removed: [session]
+      });
     }
+  }
 }
--- signal0ne/src/extension.ts
+++ signal0ne/src/extension.ts
@@ -1,83 +1,95 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import axios from 'axios';
-import { Login } from './login';
+import { Issues } from './components/issues';
+import { jwtDecode } from 'jwt-decode';
+import { Login } from './components/login';
+import { Logout } from './components/logout';
 import { Signal0neProvider } from './auth/signal0ne.provider';
-import { jwtDecode } from "jwt-decode";
-import { Issues } from './issues';
-import { Logout } from './logout';
 
-const TOKEN_REFRESH_INTERVAL = 1000 * 60;
 const ISSUES_LIST_REFRESH_INTERVAL = 1000 * 15;
+const TOKEN_REFRESH_INTERVAL = 1000 * 60;
 const TOKEN_REFRESH_TIMEOUT_THRESHOLD = 1000 * 60 * 3;
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    const signal0neProvider = new Signal0neProvider(context);
-    var issueController!: Issues;
-    var refreshIssuesInterval: NodeJS.Timeout;
-    context.subscriptions.push(
-        signal0neProvider,
-    )
+  const signal0neProvider = new Signal0neProvider(context);
+  let issueController: Issues | null;
+  let refreshIssuesInterval: NodeJS.Timeout;
 
-    setInterval(async () => {
-        const sessions = await signal0neProvider.getSessions();
-        if (sessions.length > 0) {
-            const session = sessions[0];
-            var currentToken = session.accessToken;
-            var decodedToken = jwtDecode(currentToken);
-            var tokenExpiration = decodedToken.exp ? decodedToken.exp : 0;
-            var currentTime = Math.floor(Date.now() / 1000);
-            var tokenTimeout = tokenExpiration - currentTime;
-            if (tokenTimeout < TOKEN_REFRESH_TIMEOUT_THRESHOLD) {
-                await signal0neProvider.refreshSession(session, session.scopes);
-            }
-        }           
-    }, TOKEN_REFRESH_INTERVAL)
+  context.subscriptions.push(signal0neProvider);
 
+  setInterval(async () => {
+    const sessions = await signal0neProvider.getSessions();
 
+    if (sessions.length > 0) {
+      const session = sessions[0];
+      const currentToken = session.accessToken;
+      const decodedToken = jwtDecode(currentToken);
+
+      const tokenExpiration = decodedToken.exp || 0;
+      const currentTime = Math.floor(Date.now() / 1000);
+      const tokenTimeout = tokenExpiration - currentTime;
+
+      if (tokenTimeout < TOKEN_REFRESH_TIMEOUT_THRESHOLD) {
+        await signal0neProvider.refreshSession(session);
+      }
+    }
+  }, TOKEN_REFRESH_INTERVAL);
+
+  new Login(context, signal0neProvider);
+
+  signal0neProvider.onDidAuthenticate(() => {
+    new Logout(context, signal0neProvider);
+
+    if (!issueController) {
+      issueController = new Issues(context, signal0neProvider);
+
+      refreshIssuesInterval = setInterval(async () => {
+        issueController?.issuesViewDataProvider.refresh();
+      }, ISSUES_LIST_REFRESH_INTERVAL);
+    }
+  });
+
+  signal0neProvider.onDidLogout(() => {
     new Login(context, signal0neProvider);
-    
-    signal0neProvider.onDidAuthenticate(() => {
-        new Logout(context, signal0neProvider);
-        if (!issueController) {
-            issueController = new Issues(context, signal0neProvider);
-            refreshIssuesInterval = setInterval(async () => {
-                issueController?.IssuesViewDataProvider.refresh();
-            }, ISSUES_LIST_REFRESH_INTERVAL);
-        }
-    });
 
-    signal0neProvider.onDidLogout(() => {
-        new Login(context, signal0neProvider);
-        issueController = null as any;
-        clearInterval(refreshIssuesInterval);
-    })
- 
-    vscode.commands.registerCommand('signal0ne.fixCode', async () => {
-        let editor = vscode.window.activeTextEditor;
-        let selection = editor?.selection;
-        let codeSnippet = editor?.document.getText(selection);
-        let lang = editor?.document.languageId;
- 		let codeSnippetContext = {
- 			code: codeSnippet,
- 			lang: lang
- 		}
-        if (!issueController) {
-            vscode.window.showErrorMessage('Please login to Signal0ne first');
-        }
-        var newCode = await issueController.fixCode(codeSnippetContext);
-        if (newCode === "") {
-            vscode.window.showErrorMessage('Failed to fix code');
-            return;
-        }
-        editor?.edit(editBuilder => {
-            editBuilder.replace(selection ?? new  vscode.Selection(0,0,0,0) , newCode);
-        });
+    issueController = null;
+    clearInterval(refreshIssuesInterval);
+  });
+
+  vscode.commands.registerCommand('signal0ne.fixCode', async () => {
+    const editor = vscode.window.activeTextEditor;
+
+    if (!editor) return;
+
+    const { document, edit, selection } = editor;
+    const code = document.getText(selection);
+    const lang = document.languageId;
+
+    const codeSnippetContext = {
+      code,
+      lang
+    };
+
+    if (!issueController) {
+      vscode.window.showErrorMessage('Please login to Signal0ne first');
+      return;
+    }
+
+    const newCode = await issueController.fixCode(codeSnippetContext);
+
+    if (newCode === '') {
+      vscode.window.showErrorMessage(
+        'Failed to fix the selected code. Please try again with different code snippet.'
+      );
+      return;
+    }
+
+    edit(editBuilder => {
+      editBuilder.replace(
+        selection ?? new vscode.Selection(0, 0, 0, 0),
+        newCode
+      );
     });
+  });
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
